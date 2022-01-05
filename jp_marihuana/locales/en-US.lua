7c910b4593726bbcf70a115fc73345745b6720c7
Jorgepgdb <97137622+jorgepgdb@users.noreply.github.com>
2022-01-05 02:31:21 +0100

    Add files via upload

---
+++ jp_marihuana/locales/en-US.lua
@@ -0,0 +1,31 @@
+Locales['en-US'] = {
+    planting_text = 'Plantando',
+    planting_ok = '¡Plantación realizada!',
+    planting_in_vehicle = '¿Quieres plantar marihuana en tu coche? ¿Es en serio?',
+    planting_too_steep = 'Este es un lugar muy empinado, busca otro amigo!',
+    planting_too_far = 'Un poco más cerca... por favor',
+    planting_not_suitable_soil = 'Acá no puedes plantar, dos dedos de frente por favor!',
+    planting_too_close = '¡Las plantas necesitan más espacio!',
+    planting_obstructed = 'Busca un lugar con más luz',
+    planting_no_seed = 'No tienes semillas para plantar!',
+    planting_failed = '~r~Hubo un error mientras plantabas, la planta se ha marchitado',
+    planting_too_fast = '¡Tranqui! No vayas tan rápido, se paciente',
+
+    status_active = 'Estado ~1~/~1~~n~~INPUT_PICKUP~ ~a~~n~~INPUT_COVER~ Destruir',
+    status_passive = 'Estado ~1~/~1~~n~~a~~n~~INPUT_COVER~ Destruir',
+
+    growth_seedling = 'Recién plantada.~n~Necesitará atención pronto!',
+    growth_tend = 'Atender planta',
+    growth_growing = 'Creciendo en buen estado.',
+    growth_yield = 'Cosechar',
+
+    command_invalid = 'Comando inválido: %s',
+    command_empty = 'Versión %s',
+
+    interact_text = 'Interacción planta',
+    interact_tended = 'Plantación atendida',
+    interact_destroyed = 'Plantación destuida',
+    interact_harvested = 'Plantación cosechada: %i marihuana, %i semillas',
+    interact_full = '¡No puedes llevar más marihuana!',
+    interact_missing_item = 'No tienes lo que necesitas para cuidar esta planta!',
+}
